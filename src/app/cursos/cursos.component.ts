f8be95ab7ff0584a22abd5357b5f1c1ba0259da7
Thiago Orlandini <thiagorlandini@hotmail.com>
2018-02-24 16:55:51 -0300

    curso bloco1

---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal:string;
+  cursos: string[];
+
+  constructor(private cursosService:CursosService) { 
+    this.nomePortal = 'http://loianetraining.com';
+    this.cursos = this.cursosService.getCursos();
+  }
+
+  ngOnInit() {
+  }
+
+}
